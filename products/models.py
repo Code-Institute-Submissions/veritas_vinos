08b053c6c69507c5719077991f089e7c3cda01c5
lemon-squeezy <AdrianLaub@aol.com>
2021-01-22 13:50:19 +0000

    Customized admin

---
+++ products/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    taste = models.CharField(max_length=30)
+    winery = models.CharField(max_length=30)
+    country = models.CharField(max_length=20)
+    grape_variety = models.CharField(max_length=30)
+    alcohol = models.CharField(max_length=254)
+    residual_sugar = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
