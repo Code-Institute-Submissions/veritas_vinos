f07815f0fc7bd08110808162a068d79beebc8d1f
lemon-squeezy <AdrianLaub@aol.com>
2021-01-23 17:37:45 +0000

    Added sorting JS and back to top link

--- products/views.py
+++ products/views.py
@@ -1,7 +1,9 @@
-
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Product, Category
 
 # Create your views here.
@@ -11,7 +13,7 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
-    categories = None
+    categories = Category.objects.all() 
     sort = None
     direction = None
 
@@ -22,7 +24,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -51,7 +54,7 @@ def all_products(request):
         'current_categories': categories,
         'current_sorting': current_sorting,
     }
-
+    
     return render(request, 'products/products.html', context)
 
 
@@ -63,5 +66,5 @@ def product_detail(request, product_id):
     context = {
         'product': product,
     }
-
+    
     return render(request, 'products/product_detail.html', context)
