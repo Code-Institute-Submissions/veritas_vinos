a5d60bb7e27a704c460b0e86a8c97ff9cc7097df
lemon-squeezy <AdrianLaub@aol.com>
2021-01-25 18:47:54 +0000

    Shopping bag functionality and Product view updated

--- products/views.py
+++ products/views.py
@@ -10,13 +10,12 @@ from .models import Product, Category
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
-
     products = Product.objects.all()
     query = None
-    categories = Category.objects.all() 
+    all_categories = Category.objects.all() 
     sort = None
     direction = None
-
+    current_categories = []
     if request.GET:
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
@@ -31,30 +30,25 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-            
         if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            products = products.filter(category__name__in=categories)
-            categories = Category.objects.filter(name__in=categories)
-
+            current_categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=current_categories)
+            current_categories = Category.objects.filter(name__in=current_categories)
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-
     current_sorting = f'{sort}_{direction}'
-
     context = {
         'products': products,
         'search_term': query,
-        'current_categories': categories,
+        'current_categories': current_categories,
+        'all_categories': all_categories,
         'current_sorting': current_sorting,
     }
-    
     return render(request, 'products/products.html', context)
 
 
