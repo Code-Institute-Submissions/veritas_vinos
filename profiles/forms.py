0d322ca637ff259bf46eccd747bf68365fc1fe64
lemon-squeezy <AdrianLaub@aol.com>
2021-01-29 17:57:51 +0000

    Updated webhook handler to hande profiles

--- profiles/forms.py
+++ profiles/forms.py
@@ -15,6 +15,7 @@ class UserProfileForm(forms.ModelForm):
         """
         super().__init__(*args, **kwargs)
         placeholders = {
+            'full_name': 'full_name',
             'default_phone_number': 'Phone Number',
             'default_postcode': 'Postal Code',
             'default_town_or_city': 'Town or City',
