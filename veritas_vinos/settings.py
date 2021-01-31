4b14df93c926f64f54f3b83922e2275df6e4c397
lemon-squeezy <AdrianLaub@aol.com>
2021-01-31 19:28:31 +0000

    Blog app added and styling

--- veritas_vinos/settings.py
+++ veritas_vinos/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -49,6 +48,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
+    'blog',
 
     # Other
     'crispy_forms',
