9f731c631af2a2319838de197b92f49f48dddc30
lemon-squeezy <AdrianLaub@aol.com>
2021-02-01 13:28:58 +0000

    Heroku Deployment 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn veritas-vinos.wsgi:application
--- veritas_vinos/settings.py
+++ veritas_vinos/settings.py
@@ -26,9 +26,7 @@ SECRET_KEY = 'sc(n$j13t9qn+%&56(&_ty+6bkgcxno^6zfh1rl)a-4a6htmo+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost',
-]
+ALLOWED_HOSTS = ['veritas-vinos.herokuapp.com','localhost']
 
 
 # Application definition
@@ -122,12 +120,17 @@ WSGI_APPLICATION = 'veritas_vinos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
