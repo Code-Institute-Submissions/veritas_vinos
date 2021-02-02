1c4edcbddd41d5d2cd0439f835e4a6468e130bbb
lemon-squeezy <AdrianLaub@aol.com>
2021-02-02 12:25:36 +0000

    created datadump for a new database

--- veritas_vinos/settings.py
+++ veritas_vinos/settings.py
@@ -121,17 +121,12 @@ WSGI_APPLICATION = 'veritas_vinos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 
 # Password validation
